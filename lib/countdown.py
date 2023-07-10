cb0449cf4d68f6611e79489cc14b19e6fbe7992b
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-10 17:32:00 -0400

    lab complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
+import time
 # your code goes here!
+def countdown(int):
+    while int > 0:
+        print(f'{int} SECOND(S)!')
+        int -= 1
+    if int == 0:
+        print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(int):
+    while int > 0:
+        print(f'{int} SECOND(S)!')
+        int -= 1
+        time.sleep(1)
+    if int == 0:
+        print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(15)
